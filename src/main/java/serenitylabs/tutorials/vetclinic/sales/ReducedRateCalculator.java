4e2466fac24f3d2e4e04a2587e65018963797a40
Marco Tedone <marco.tedone@gmail.com>
2018-04-03 20:20:03 +0100

    Improved ReducedTaxCalculator@

--- src/main/java/serenitylabs/tutorials/vetclinic/sales/ReducedRateCalculator.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/sales/ReducedRateCalculator.java
@@ -5,6 +5,12 @@ import serenitylabs.tutorials.vetclinic.sales.model.LineItem;
 public class ReducedRateCalculator implements TaxRateCalculator {
     @Override
     public TaxRate rateFor(LineItem item) {
-        return new TaxRate(0.09, "Reduced");
+        double rate = 0.09;
+
+        if (item.getTotal() > 100) {
+            rate = 0.135;
+        }
+
+        return new TaxRate(rate, "Reduced");
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/sales/WhenApplyingSalesTax.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/sales/WhenApplyingSalesTax.java
@@ -11,6 +11,7 @@ import static serenitylabs.tutorials.vetclinic.sales.model.ProductCategory.Snack
 public class WhenApplyingSalesTax {
 
     private final static double NINE_PERCENT = 0.09;
+    private final static double THIRTHEEN_POINT_FIVE_PERCENT = 0.135;
 
     @Test
     public void crisps_should_be_charged_at_the_reduced_rate() {
@@ -26,6 +27,28 @@ public class WhenApplyingSalesTax {
 
         // THEN
         SalesTax expectedSalesTax = SalesTax.atRateOf(NINE_PERCENT).withName("Reduced").forAnAmountOf(0.27);
+
+        assertThat(calculatedSalesTax, equalTo(expectedSalesTax));
+
+    }
+
+    @Test
+    public void over_100_euros_of_crisps_is_charged_at_a_higher_price() {
+
+        // GIVEN
+        LineItem crisps = LineItem.forASaleOf(50)
+                .itemCalled("salt and vinegar crisps")
+                .inCategory(Snacks)
+                .withAUnitPriceOf(3.00);
+
+        // WHEN
+        SalesTaxService salesTaxService = new SalesTaxService();
+        SalesTax calculatedSalesTax = salesTaxService.salesTaxEntryFor(crisps);
+
+        // THEN
+        SalesTax expectedSalesTax = SalesTax.atRateOf(THIRTHEEN_POINT_FIVE_PERCENT)
+                .withName("Reduced")
+                .forAnAmountOf(20.25);
 
         assertThat(calculatedSalesTax, equalTo(expectedSalesTax));
 
